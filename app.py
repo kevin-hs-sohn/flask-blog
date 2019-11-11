0870c7eb55701180796cc1db5f8a22826e907dc6
손현석 <kevinsohn@sonhyeonseog-ui-MacBookPro.local>
2019-11-11 22:34:08 +0900

    first_post

--- app.py
+++ app.py
@@ -4,5 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', user='kevin')
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/post/1')
+def post():
+    return render_template('first_post.html')
